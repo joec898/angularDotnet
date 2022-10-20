b842fe6688a72b0eaf67b98428cbc029a4897992
joe c <joec898@gmail.com>
2022-10-20 16:53:00 -0500

    initial commit

---
+++ angular/src/app/employee/show-emp/show-emp.component.ts
@@ -0,0 +1,100 @@
+import { Component, OnInit } from '@angular/core';
+import {SharedService} from 'src/app/shared.service';
+
+@Component({
+  selector: 'app-show-emp',
+  templateUrl: './show-emp.component.html',
+  styleUrls: ['./show-emp.component.css']
+})
+export class ShowEmpComponent implements OnInit {
+
+  constructor(private service:SharedService) { }
+
+  EmployeeList:any=[];
+
+  ModalTitle:string;
+  ActivateAddEditEmpComp:boolean=false;
+  emp:any;
+
+  EmployeeIdFilter:string="";
+  EmployeeNameFilter:string="";
+  EmployeeDepFilter:string="";
+  EmployeeListWithoutFilter:any=[];
+
+
+  ngOnInit(): void {
+    this.refreshEmpList();
+  }
+
+  addClick(){
+    this.emp={
+      EmployeeId:0,
+      EmployeeName:"",
+      Employee:"",
+      DateOfJoining:"",
+      PhotoFileName:"anonymous.png"
+    }
+    this.ModalTitle="Add Employee";
+    this.ActivateAddEditEmpComp=true;
+
+  }
+
+  editClick(item){
+    console.log(item);
+    this.emp=item;
+    this.ModalTitle="Edit Employee";
+    this.ActivateAddEditEmpComp=true;
+  }
+
+  deleteClick(item){
+    if(confirm('Are you sure??')){
+      this.service.deleteEmployee(item.EmployeeId).subscribe(data=>{
+        alert(data.toString());
+        this.refreshEmpList();
+      })
+    }
+  }
+
+  closeClick(){
+    this.ActivateAddEditEmpComp=false;
+    this.refreshEmpList();
+  }
+
+
+  refreshEmpList(){
+    this.service.getEmpList().subscribe(data=>{
+      this.EmployeeList=data;
+      this.EmployeeListWithoutFilter=data;
+    });
+  }
+
+  filterFn(){
+    var EmployeeIdFilter = this.EmployeeIdFilter;
+    var EmployeeNameFilter = this.EmployeeNameFilter;
+    var EmployeeDepFilter = this.EmployeeDepFilter;
+
+    this.EmployeeList = this.EmployeeListWithoutFilter.filter(function (el){
+        return el.EmployeeId.toString().toLowerCase().includes(
+          EmployeeIdFilter.toString().trim().toLowerCase()
+        )&&
+        el.EmployeeName.toString().toLowerCase().includes(
+          EmployeeNameFilter.toString().trim().toLowerCase()
+        )&&
+        el.Department.toString().toLowerCase().includes(
+          EmployeeDepFilter.toString().trim().toLowerCase()
+        )
+    });
+  }
+
+  sortResult(prop,asc){
+    this.EmployeeList = this.EmployeeListWithoutFilter.sort(function(a,b){
+      if(asc){
+          return (a[prop]>b[prop])?1 : ((a[prop]<b[prop]) ?-1 :0);
+      }else{
+         return (b[prop]>a[prop])?1 : ((b[prop]<a[prop]) ?-1 :0);
+      }
+    })
+  }
+
+}
+
